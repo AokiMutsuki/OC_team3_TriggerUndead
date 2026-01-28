d3cca3bba898d26fe072c4813099c742cf0930a4
Aoki Mutsuki <CA01254001@st.kawahara.ac.jp>
2026-01-28 11:05:32 +0900

    ゲーム背景の追加

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Game.h"
 #include "Player.h"
 #include "FPSCamera.h"
@@ -10,6 +10,9 @@ Game::Game()
 	m_fpscamera = NewGO<FPSCamera>(0, "FPSCamera");
 	m_stage = NewGO<Stage>(0, "stage");
 
+	m_skyCube = NewGO<SkyCube>(0, "SkyCube");
+	m_skyCube->SetScale(Vector3::One * 1000.0f);
+	m_skyCube->SetType(enSkyCubeType_NightToon_2);
 }
 
 Game::~Game()
